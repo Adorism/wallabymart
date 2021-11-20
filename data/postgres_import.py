1ecf28e4f10ab12b5877135b4fb146b911a9a362
Audrey Maldonado <audrey.maldonado@gmail.com>
2021-11-19 19:10:12 -0500

    Update postgres_import.py

--- data/postgres_import.py
+++ data/postgres_import.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 import pandas as pd
 df = pd.read_csv('wallabymart_customer_data.csv')
 # postgres doesn't like capitals or spaces
+# that's probably my answer
 df.columns = [c.lower() for c in df.columns]
 
 engine = create_engine(
